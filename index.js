da79b74345bafa233af8f906ddc1761093391b80
Arbauman <arbauman207@gmail.com>
2016-10-28 05:44:02 -0400

    Some misc. cleanup
    Replaced vars
    Added some const where it seemed to make sense
    And so, so very many semicolons.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let Eris = require('eris')
-let fs = require('fs')
-let request = require('request')
-let merge = require('merge')
-let reload = require('require-reload')(require)
+const Eris = require('eris');
+const fs = require('fs');
+const request = require('request');
+const merge = require('merge');
+const reload = require('require-reload')(require);
 
-let config = require('./config.json')
+let config = require('./config.json');
 
 let guilds
 try {
-    guilds = reload('./guilds.json')
+    guilds = reload('./guilds.json');
 } catch (e) {
-    console.log('BAD THING HAPPENED AND GUILDS.JSON DOESNT EXIST AAAAAAAA')
-    guilds = {}
+    console.log('BAD THING HAPPENED AND GUILDS.JSON DOESNT EXIST AAAAAAAA');
+    guilds = {};
 }
 
 let c = new Eris.CommandClient(config.token, { userAccount: false }, {
@@ -23,25 +23,25 @@ let c = new Eris.CommandClient(config.token, { userAccount: false }, {
     }
 })
 
-let sharedStream = c.createSharedStream(config.stream)
+let sharedStream = c.createSharedStream(config.stream);
 
 function joinVoice (client, guild, channel) { // Join a voice channel and start playing the stream there
     let cc = client.voiceConnections.guilds[guild] // Find a current connection in this guild
     if (cc) { // If there is one
         // Just switch the channel for this connection
-        cc.switchChannel(channel)
+        cc.switchChannel(channel);
         // Config thing
-        writeGuildConfig(guild, {vc: channel})
+        writeGuildConfig(guild, {vc: channel});
     } else { // Looks like we'll need to make a new one
         // Create a new voice connection and join the channel
         client.joinVoiceChannel(channel, { shared: true }).then(vc => {
             if (vc) {
-                sharedStream.voiceConnections.add(vc)
-                let realGuild = c.guilds.get(guild)
-                console.log(`Added voice connection for guild ${realGuild.name} (${realGuild.id})`)
+                sharedStream.voiceConnections.add(vc);
+                let realGuild = c.guilds.get(guild);
+                console.log(`Added voice connection for guild ${realGuild.name} (${realGuild.id})`);
             }
         }).catch(error => {
-            console.log(error)
+            console.log(error);
         })
     }
 
@@ -49,23 +49,23 @@ function joinVoice (client, guild, channel) { // Join a voice channel and start 
 }
 
 function writeGuildConfig (guild, object) { // Change a guild's config via an object of options, and save the changes
-    var currentConfig = guilds[guild] || {} // Get gurrent config for this guild, creating it if it doesn't exist
-    var newConfig = merge(currentConfig, object) // Merge new options with current
-    var _guilds = guilds
-    _guilds[guild] = newConfig // Write this new config back to the config
+    const currentConfig = guilds[guild] || {} // Get gurrent config for this guild, creating it if it doesn't exist
+    const newConfig = merge(currentConfig, object); // Merge new options with current
+    let _guilds = guilds;
+    _guilds[guild] = newConfig; // Write this new config back to the config
     if (!fs.existsSync('./backups'))
-        fs.mkdirSync('./backups')
+        fs.mkdirSync('./backups');
     fs.writeFile(`backups/guilds-${Date.now()}.json`, JSON.stringify(guilds)) // Create a backup before doing anything
     fs.writeFile('guilds.json', JSON.stringify(_guilds), 'utf-8', err => { // Store the new stuff in the file
-        if (err) console.log(err)
-        else guilds = reload('./guilds.json') // Reload the file
+        if (err) console.log(err);
+        else guilds = reload('./guilds.json'); // Reload the file
     })
 }
 
 function getGuildConfig (guild, option) { // Get a config option from a guild
-    let defaults = config.guildDefaults // Grab the defaults, just in case
+    let defaults = config.guildDefaults; // Grab the defaults, just in case
     if (!guilds[guild] || !guilds[guild][option]) return defaults[option] // logic whee
-    return guilds[guild][option]
+    return guilds[guild][option];
 }
 
 function getSongInfo (callback) { // Get the stream's info for the current song
@@ -74,7 +74,7 @@ function getSongInfo (callback) { // Get the stream's info for the current song
         // \o/
         if (!err) return callback(null, body)
         // shit
-        return callback(err)
+        return callback(err);
     })
 }
 
@@ -84,70 +84,70 @@ function memberHasManageGuild (member) { // Return whether or not the user can m
 
 c.once('ready', () => {
     let errorHandler = (e) => {
-        console.log('SharedStream died!')
+        console.log('SharedStream died!');
         if (e) {
             if (typeof e === 'string')
-                console.log(e)
+                console.log(e);
             else
-                console.log(JSON.stringify(e))
+                console.log(JSON.stringify(e));
         }
-        process.exit(1) // Kill ourself if the stream died, so our process monitor can restart us
+        process.exit(1); // Kill ourself if the stream died, so our process monitor can restart us
         // hey anon suicide is bad okay
         // please anon dont k?
     }
-    sharedStream.on('error', errorHandler)
-    sharedStream.on('end', errorHandler)
+    sharedStream.on('error', errorHandler);
+    sharedStream.on('end', errorHandler);
     sharedStream.on('disconnect', (vc) => {
-        console.log(':( - Disconnected from ' + vc.id)
+        console.log(':( - Disconnected from ' + vc.id);
     })
-	sharedStream.play()
+	sharedStream.play();
 
-    console.log(`Connected as ${c.user.username} / Currently in ${c.guilds.size} servers`)
+    console.log(`Connected as ${c.user.username} / Currently in ${c.guilds.size} servers`);
 
     // This code has no practical value, but it's fun so w/e
     function gameCurrentSong () {
         getSongInfo((err, body) => {
             if (!err) {
-                c.editStatus({name: `${body.artist_name} ${config.separator || '-'} ${body.song_name}`})
+                c.editStatus({name: `${body.artist_name} ${config.separator || '-'} ${body.song_name}`});
             } else {
-                c.editStatus({name: 'music probably'})
-                console.log("Getting song info didn't work\n"+err)
+                c.editStatus({name: 'music probably'});
+                console.log("Getting song info didn't work\n"+err);
             }
         })
 
-        setTimeout(gameCurrentGuilds, 15000)
+        setTimeout(gameCurrentGuilds, 15000);
     }
 
     function gameCurrentGuilds () {
-        c.editStatus({name: `on ${c.guilds.size} servers`})
-        setTimeout(gameCurrentUsers, 5000)
+        c.editStatus({name: `on ${c.guilds.size} servers`});
+        setTimeout(gameCurrentUsers, 5000);
     }
 
     function gameCurrentUsers () {
-        let userCount = 0
+        let userCount = 0;
         // For every guild
         c.guilds.forEach(g => {
             // For every channel that is a voice channel and we're in
             g.channels.filter(d => d.voiceMembers ? d.voiceMembers.get('222167140004790273') : false).forEach(d => {
                 // Add the number of members in this channel, not counting ourself
-                userCount += d.voiceMembers.size - 1
-            })
-        })
+                userCount += d.voiceMembers.size - 1;
+            });
+        });
 
-        c.editStatus({name: `for ${userCount} listeners`})
-        setTimeout(gameCurrentSong, 5000)
+        c.editStatus({name: `for ${userCount} listeners`});
+        setTimeout(gameCurrentSong, 5000);
     }
 
     gameCurrentSong(); // Let's fire up the non-practical value but fun code
-    
+
     // end useless code - begin code that does useful things
     // (I could get into an argument about relative usefulness here but I'll leave that for another unnecessary comment)
     for (let guild of Object.keys(guilds)) { // loop through all the servers recorded
-        let channel = getGuildConfig(guild, 'vc') // Get the channel for this guild
-        let prefix = getGuildConfig(guild, 'prefix') // Get the prefix for this guild
+        const channel = getGuildConfig(guild, 'vc'); // Get the channel for this guild
+        const prefix = getGuildConfig(guild, 'prefix'); // Get the prefix for this guild
 
-        if (channel) joinVoice(c, guild, channel) // Connect and play if there's one set
-        if (prefix) c.registerGuildPrefix(guild, prefix) // also this
+        if (channel) joinVoice(c, guild, channel); // Connect and play if there's one set
+        if (prefix) c.registerGuildPrefix(guild, prefix); // also this
     }
 })
 
@@ -156,18 +156,18 @@ c.registerCommand('join', msg => {
 
     // Join command - joins the VC the user is in, and sets that as the music channel for the server
     // Requires manage server; can't be used in PM
-    if (!memberHasManageGuild(msg.member)) return
+    if (!memberHasManageGuild(msg.member)); return
 
-    let member = msg.member
-    let channelId = member.voiceState ? member.voiceState.channelID : null
+    const member = msg.member;
+    const channelId = member.voiceState ? member.voiceState.channelID : null;
     if (!channelId) {
         // fail
-        c.createMessage(msg.channel.id, 'Join a voice channel first!')
+        c.createMessage(msg.channel.id, 'Join a voice channel first!');
     } else {
         // oh dang hello
-        writeGuildConfig(msg.channel.guild.id, {vc: channelId})
-        joinVoice(c, msg.channel.guild.id, channelId)
-        c.createMessage(msg.channel.id, '\\o/')
+        writeGuildConfig(msg.channel.guild.id, {vc: channelId});
+        joinVoice(c, msg.channel.guild.id, channelId);
+        c.createMessage(msg.channel.id, '\\o/');
     }
 
 })
@@ -176,21 +176,21 @@ c.registerCommand('prefix', (msg, args) => {
 
     // Prefix command - Change's the bot's prefix in the server
     // Requires manage server; can't be used in PM
-    if (!memberHasManageGuild(msg.member)) return
+    if (!memberHasManageGuild(msg.member)) return;
 
     if(args[0] === undefined){
-        c.createMessage(msg.channel.id, 'You must provide a new prefix')
-        return
+        c.createMessage(msg.channel.id, 'You must provide a new prefix');
+        return;
     }
 
-    let newPrefix = args[0]
+    let newPrefix = args[0];
     if (/[a-zA-Z0-9\s\n]/.test(newPrefix)) {
-        c.createMessage(msg.channel.id, "Invalid prefix. Can't be a letter, number, or whitespace character.")
-        return
+        c.createMessage(msg.channel.id, "Invalid prefix. Can't be a letter, number, or whitespace character.");
+        return;
     }
-    writeGuildConfig(msg.channel.guild.id, {prefix: newPrefix})
-    c.registerGuildPrefix(msg.channel.guild.id, newPrefix)
-    c.createMessage(msg.channel.id, '\\o/')
+    writeGuildConfig(msg.channel.guild.id, {prefix: newPrefix});
+    c.registerGuildPrefix(msg.channel.guild.id, newPrefix);
+    c.createMessage(msg.channel.id, '\\o/');
 
 })
 
@@ -198,16 +198,16 @@ c.registerCommand('ignore', msg => {
 
     // Ignore command - ignores user commands in this channel
     // Requires manage server; can't be used in PM
-    if (!memberHasManageGuild(msg.member)) return
+    if (!memberHasManageGuild(msg.member)) return;
 
-    let denied = getGuildConfig(msg.channel.guild.id, 'denied')
+    let denied = getGuildConfig(msg.channel.guild.id, 'denied');
     if (!denied.includes(msg.channel.id)) {
         denied.push(msg.channel.id)
-        writeGuildConfig(msg.channel.guild.id, {denied: denied})
-        c.createMessage(msg.channel.id, "All right, I'll ignore this channel now.")
+        writeGuildConfig(msg.channel.guild.id, {denied: denied});
+        c.createMessage(msg.channel.id, "All right, I'll ignore this channel now.");
     } else {
-        c.createMessage(msg.channel.id, "I'm already ignoring this channel.")
-        return
+        c.createMessage(msg.channel.id, "I'm already ignoring this channel.");
+        return;
     }
 })
 
@@ -215,16 +215,16 @@ c.registerCommand('unignore', msg => {
 
     // Unignore command - Stops ignoring user commands in this channel
     // Requires manage server; can't be used in PM
-    if (!memberHasManageGuild(msg.member)) return
+    if (!memberHasManageGuild(msg.member)) return;
 
-    let denied = getGuildConfig(msg.channel.guild.id, 'denied')
+    let denied = getGuildConfig(msg.channel.guild.id, 'denied');
     if (denied.includes(msg.channel.id)) {
-        denied.splice(denied.indexOf(msg.channel.id), 1)
-        writeGuildConfig(msg.channel.guild.id, {denied: denied})
-        c.createMessage(msg.channel.id, "Got it! I'll stop ignoring this channel.")
+        denied.splice(denied.indexOf(msg.channel.id), 1);
+        writeGuildConfig(msg.channel.guild.id, {denied: denied});
+        c.createMessage(msg.channel.id, "Got it! I'll stop ignoring this channel.");
     } else {
-        c.createMessage(msg.channel.id, "I wasn't ignoring this channel.")
-        return
+        c.createMessage(msg.channel.id, "I wasn't ignoring this channel.");
+        return;
     }
 
 })
@@ -233,14 +233,14 @@ c.registerCommand('ignoreall', msg => {
 
     // Ignore all command - Ignores all text channels in a guild
     // Requires manage server; can't be used in PM
-    if (!memberHasManageGuild(msg.member)) return
+    if (!memberHasManageGuild(msg.member)) return;
 
-    let denied = []
-    let guildObj = c.guilds.find(g => g.id === msg.channel.guild.id)
-    let textChannelIds = guildObj.channels.filter(c => c.type === 0).map(c => c.id)
-    textChannelIds.forEach(c => denied.push(c))
-    writeGuildConfig(msg.channel.guild.id, {denied: denied})
-    c.createMessage(msg.channel.id, "I'm now ignoring every channel in the server.")
+    let denied = [];
+    let guildObj = c.guilds.find(g => g.id === msg.channel.guild.id);
+    let textChannelIds = guildObj.channels.filter(c => c.type === 0).map(c => c.id);
+    textChannelIds.forEach(c => denied.push(c));
+    writeGuildConfig(msg.channel.guild.id, {denied: denied});
+    c.createMessage(msg.channel.id, "I'm now ignoring every channel in the server.");
 
 })
 
@@ -248,10 +248,10 @@ c.registerCommand('unignoreall', msg => {
 
     // Unignore all command - stops ignoring all text channels
     // Requires manage server; can't be used in PM
-    if (!memberHasManageGuild(msg.member)) return
+    if (!memberHasManageGuild(msg.member)) return;
 
-    writeGuildConfig(msg.channel.guild.id, {denied: []})
-    c.createMessage(msg.channel.id, "I'm no longer ignoring any channels here.")
+    writeGuildConfig(msg.channel.guild.id, {denied: []});
+    c.createMessage(msg.channel.id, "I'm no longer ignoring any channels here.");
 
 })
 
@@ -260,44 +260,44 @@ c.registerCommand('np', msg => {
     if (getGuildConfig(msg.channel.guild.id, 'denied').includes(msg.channel.id)) return // Do nothing if this channel is ignored
     getSongInfo((err, info) => {
         if (!err) {
-            let requestby = info.request ? `\n**Requested by:** ${info.requested_by} (<https://forum.listen.moe/u/${info.requested_by}>)` : ''
-            let anime = info.anime_name ? `\n**Anime:** ${info.anime_name}` : ''
-            c.createMessage(msg.channel.id, `**Now playing:** "${info.song_name}" by ${info.artist_name}${requestby}${anime}`)
+            let requestby = info.request ? `\n**Requested by:** ${info.requested_by} (<https://forum.listen.moe/u/${info.requested_by}>)` : '';
+            let anime = info.anime_name ? `\n**Anime:** ${info.anime_name}` : '';
+            c.createMessage(msg.channel.id, `**Now playing:** "${info.song_name}" by ${info.artist_name}${requestby}${anime}`);
         }
     })
 
-}, { aliases: ['playing', 'nowplaying'] })
+}, { aliases: ['playing', 'nowplaying'] });
 
 c.registerCommand('eval', (msg, args) => {
     if (!config.owners.includes(msg.author.id)) return c.createMessage(msg.channel.id, 'soz bae must be bot owner') // todo: stop using unnecessary todo lines that make lines way too long
-    let thing
+    let thing;
     try {
-        thing = eval(args[0]) // eval is harmful my ass
+        thing = eval(args[0]); // eval is harmful my ass
     } catch (e) {
-        thing = e
+        thing = e;
     }
-    c.createMessage(msg.channel.id, thing)
+    c.createMessage(msg.channel.id, thing);
 })
 
 c.registerCommand('servers', msg => {
 
-    if (!config.owners.includes(msg.author.id)) return c.createMessage(msg.channel.id, 'soz bae must be bot owner') // jkfhasdkjhfkajshdkfsf
+    if (!config.owners.includes(msg.author.id)) return c.createMessage(msg.channel.id, 'soz bae must be bot owner'); // jkfhasdkjhfkajshdkfsf
 
-    let message = c.guilds.map(g=>`\`${g.id}\` ${g.name}`).join('\n')
-    let messageLengthCap = 2000
+    let message = c.guilds.map(g=>`\`${g.id}\` ${g.name}`).join('\n');
+    const messageLengthCap = 2000;
 
-    let strs = []
+    let strs = [];
     while (message.length > messageLengthCap) {
-        let pos = message.substring(0, messageLengthCap).lastIndexOf('\n')
-        pos = pos <= 0 ? messageLengthCap : pos
-        strs.push(message.substring(0, pos))
-        let i = message.indexOf('\n', pos)+1
-        if (i < pos || i > pos+messageLengthCap) i = pos
-        message = message.substring(i)
+        let pos = message.substring(0, messageLengthCap).lastIndexOf('\n');
+        pos = pos <= 0 ? messageLengthCap : pos;
+        strs.push(message.substring(0, pos));
+        let i = message.indexOf('\n', pos)+1;
+        if (i < pos || i > pos+messageLengthCap) i = pos;
+        message = message.substring(i);
     }
-    strs.push(message)
+    strs.push(message);
 
-    for (let str of strs) c.createMessage(msg.channel.id, str)
+    for (let str of strs) c.createMessage(msg.channel.id, str);
 
     // kana was here :eyes:
 })
@@ -309,7 +309,7 @@ c.registerCommand('servers', msg => {
     });
 */
 
-c.connect()
+c.connect();
 
 /*
 
